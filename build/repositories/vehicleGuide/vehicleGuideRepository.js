4173b0ba770c1cd204b86cb6ef7c6126df8200b5
Thomas Ibarra <sparksd2145.dev@gmail.com>
2015-02-01 15:25:03 -0600

    added missing vehicle guide repo

---
+++ build/repositories/vehicleGuide/vehicleGuideRepository.js
@@ -0,0 +1,71 @@
+/**
+ * @file Vehicle Fit Guide Repository
+ * @author Thomas Ibarra <sparksd2145.dev@gmail.com>
+ */
+
+var rest = require('restler'),
+    moment = require('moment');
+
+/**
+ * Handles retrieving information from the vehicle fit guide, which is powered by Geek Squad Central.
+ * @param app Origin application passed to the repository for configuration.
+ * @constructor
+ */
+function VehicleGuideRepository(app){
+    if(!app){
+        throw new Error('The application was not passed to the Vehicle Guide Repository.');
+    }
+
+    this.app = app;
+    this.debug = app.get('debug')('VehicleGuideRepository');
+    this.url = app.get('config').vehicleGuide.url;
+}
+
+VehicleGuideRepository.prototype.getVehicle = function(year, make, model, next){
+    var apiUrl = this.url;
+
+    rest.get(apiUrl, {
+        query: {
+            year: year,
+            make: make,
+            model: model
+        }
+    }).on('success', function(result){
+        next(result);
+    });
+};
+
+VehicleGuideRepository.prototype.getVehicleModels = function(year, make, next){
+    var apiUrl = this.url;
+
+    rest.get(apiUrl, {
+        query: {
+            year: year,
+            make: make
+        }
+    }).on('success', function(result){
+        next(result);
+    });
+};
+
+VehicleGuideRepository.prototype.getVehicleMakes = function(year, next){
+    var apiUrl = this.url;
+
+    rest.get(apiUrl, {
+        query: {
+            year: year
+        }
+    }).on('success', function(result){
+        next(result);
+    });
+};
+
+VehicleGuideRepository.prototype.getVehicleYears = function(next){
+    var apiUrl = this.url;
+
+    rest.get(apiUrl).on('success', function(result){
+        next(result);
+    });
+};
+
+module.exports = VehicleGuideRepository;
